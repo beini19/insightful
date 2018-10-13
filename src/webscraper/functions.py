989d464608141197c89955e606a8b2d0eddd8716
Sherry Li <sherryhli@users.noreply.github.com>
2018-10-12 23:56:55 -0400

    Experimenting with Selenium to retrieve reviews

--- src/webscraper/functions.py
+++ src/webscraper/functions.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+def get_reviews():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.ca/maps/place/Gol's+Lanzhou+Noodle/@43.4726523,-80.5362191,21z/data=!4m5!3m4!1s0x882bf5b25a94ebb3:0x1ac4eaf8c994bd9a!8m2!3d43.4725456!4d-80.5361185")
+    numReviewsButton = driver.find_element_by_xpath("//button[@class='section-reviewchart-numreviews']")
+    numReviewsButton.click()
+    reviewWidget = driver.find_element_by_xpath("//div[@class='widget-pane-content']")
+    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', reviewWidget) # test this
+    time.sleep(20)
+    driver.close()
+    return reviews # array of Review objects
