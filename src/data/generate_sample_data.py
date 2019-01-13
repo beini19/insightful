a816a7140331e429b299c6c2ea0b29019c5a4fd2
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-13 00:22:15 -0500

    add sample data generation

---
+++ src/data/generate_sample_data.py
@@ -0,0 +1,28 @@
+import json
+from review import Review
+from pprint import pprint
+
+# populate an array of objects with review test data in the form of a json file
+# returns an array of Review objects
+def read_review_data(filename):
+    review_list = []     
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        for obj in data['reviews']:
+            review = Review()
+            review.rating = obj['rating']
+            review.date = obj['date']
+            review.text = obj['review_text']
+            review_list.append(review)
+    return review_list
+
+def print_review_list(review_list):
+    for review in review_list:
+        print("Rating: " + str(review.rating))
+        print("Date: " + str(review.date))
+        print("Text: " + review.text + "\n")
+
+filename = "sample_text.json"
+review_list = read_review_data(filename)
+
+# print_review_list(review_list)
