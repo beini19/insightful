4462b72c844c393c610d897ffe76ad7180d026f4
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-13 00:20:56 -0500

    fixed naming in Review class

--- src/data/review.py
+++ src/data/review.py
@@ -1,7 +1,7 @@
 # Contains information about individual reviews for a business
 class Review:
     def __init__(self):
-        self.rating = -1
-        self.date = ""
-        self.review_text = ""
+        self.rating = -1            # integer between 1 and 5 - number of stars
+        self.date = 0               # integer - number of days before present date              
+        self.text = ""
 
