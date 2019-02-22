292a6ebc95c72743e81ee2bda2ec50aba3908bea
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-02-21 20:56:57 -0500

    Added progress bar and styles

--- src/client/src/components/ProgressBar.jsx
+++ src/client/src/components/ProgressBar.jsx
@@ -5,46 +5,39 @@ class LineGraph extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Apples',
-		  value: 25,
+      name: this.props.name,
+		  value: this.props.value,
 		  color: '#b53471'
     }
   }
 
   render() {
     return (
-      // <div className="progress-barasdf">
-      <div>
-        <div className="progress">
-          <div className="progress-bar" role="progressbar" style={{'width': '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-        </div>
-        {/* <div className="value">
-         <div style={{'color': this.state.color, 'width': this.state.value + '%'}}>
-           <span>{this.state.value}%</span>
-         </div>
-        </div>
-        <div className="scale">
-         <div className="graduation" style={{'color': this.state.color, 'width': this.state.value + '%'}}>
-           <span>|</span>
-         </div>
-        </div>
+      <div className="progress-bar">
+        <div className="legend">
+          <div>
+            <span className="label">{this.state.name}</span>
+          </div>
+          <div className="value">
+            <div style={{'color': this.state.color}}>
+              <span>{this.state.value}%</span>
+            </div>
+          </div>
+        </div>     
+        {/* <div className="scale">
+          <div className="graduation" style={{'color': this.state.color, 'width': this.state.value + '%'}}>
+            <span>|</span>
+          </div>
+        </div> */}
         <div className="bar">
-         <div style={{'backgroundColor': this.state.color, 'width': this.state.value + '%'}}>
-         </div>
-         <div style={{'backgroundColor': '#d3d3d3', 'width': (100-this.state.value) + '%'}}>
-         </div>
-        </div>  */}
+          <div style={{'backgroundColor': this.state.color, 'width': this.state.value + '%'}}>
+          </div>
+          <div style={{'backgroundColor': '#d3d3d3', 'width': (100-this.state.value) + '%'}}>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-
-let reading = {
-		name: 'Apples',
-		value: 25,
-		color: '#b53471'
-};
-
 
 export default LineGraph;
